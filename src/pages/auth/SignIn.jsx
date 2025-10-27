ba3cce0badac0a54fdcc7f3a0cd228bc6277d95b
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-27 21:00:03 +0300

    basic sign in page structuring done

---
+++ src/pages/auth/SignIn.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+
+const SignIn = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+
+export default SignIn
