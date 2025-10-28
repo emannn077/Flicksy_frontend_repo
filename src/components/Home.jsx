0f3cc4c8f95e61eaf49b6f7dead489d8f14492e2
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-28 16:26:32 +0300

    basic home page css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,17 @@
 import { useNavigate } from "react-router-dom"
 import { NavLink } from "react-router-dom"
+import "/public/stylesheet/style.css"
+
 const Home = () => {
   let navigate = useNavigate()
 
   return (
     <>
       <NavLink />
-      <div>
+
+      <div className="home-page">
+        <h1>Snap your day, slay your day.</h1>
+        <h3> Share your life unfiltered</h3>
         <div className="signin-homepage">
           <button onClick={() => navigate("/sign-in")}>
             Get Started With Flicksy
